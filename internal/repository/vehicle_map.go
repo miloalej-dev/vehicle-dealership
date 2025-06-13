f76216814a5babc46544d3dfa8dee3ca1b23df1b
Camilo Nossa <camilo.nossa@mercadolibre.com.co>
2025-06-13 15:18:01 -0500

    Add vehicle retrieval and management methods to VehicleMap and VehicleRepository

--- internal/repository/vehicle_map.go
+++ internal/repository/vehicle_map.go
@@ -1,6 +1,7 @@
 package repository
 
 import (
+	"errors"
 	"github.com/miloalej-dev/vehicle-dealership/pkg/models"
 )
 
@@ -29,5 +30,158 @@ func (r *VehicleMap) FindAll() (v map[int]models.Vehicle, err error) {
 		v[key] = value
 	}
 
+	return
+}
+
+// FindByID is a method that returns a vehicle by its ID
+func (r *VehicleMap) FindByID(id int) (v models.Vehicle, err error) {
+	v, ok := r.db[id]
+	if !ok {
+		err = errors.New("vehicle not found")
+		return
+	}
+	return
+}
+
+// FindByBrand is a method that returns a map of vehicles by brand
+func (r *VehicleMap) FindByBrand(brand string) (v map[int]models.Vehicle, err error) {
+	for key, vehicle := range r.db {
+		if vehicle.Brand == brand {
+			v[key] = vehicle
+		}
+	}
+	return
+}
+
+// FindByTransmission is a method that returns a map of vehicles by transmission
+func (r *VehicleMap) FindByTransmission(transmission string) (v map[int]models.Vehicle, err error) {
+	for key, vehicle := range r.db {
+		if vehicle.Transmission == transmission {
+			v[key] = vehicle
+		}
+	}
+	return
+}
+
+// FindByColorAndYear is a method that returns a map of vehicles by color and year
+func (r *VehicleMap) FindByColorAndYear(color string, year int) (v map[int]models.Vehicle, err error) {
+	for key, vehicle := range r.db {
+		if vehicle.Color != color && vehicle.FabricationYear != year {
+			v[key] = vehicle
+		}
+	}
+	return
+}
+
+// FindByBrandAndBetweenYears is a method that returns a map of vehicles by brand and between years
+func (r *VehicleMap) FindByBrandAndBetweenYears(brand string, startYear, endYear int) (v map[int]models.Vehicle, err error) {
+	for key, vehicle := range r.db {
+		if vehicle.Brand == brand && vehicle.FabricationYear >= startYear && vehicle.FabricationYear <= endYear {
+			v[key] = vehicle
+		}
+	}
+	return
+}
+
+// FindByFuelType is a method that returns a map of vehicles by fuel type
+func (r *VehicleMap) FindByFuelType(fuel string) (v map[int]models.Vehicle, err error) {
+	for key, vehicle := range r.db {
+		if vehicle.FuelType == fuel {
+			v[key] = vehicle
+		}
+	}
+	return
+}
+
+// FindByDimensions is a method that returns a map of vehicles by dimensions
+func (r *VehicleMap) FindByDimensions(minLength, maxLength, minWidth, maxWidth float64) (v map[int]models.Vehicle, err error) {
+	for key, vehicle := range r.db {
+		if vehicle.Length >= minLength && vehicle.Length <= maxLength && vehicle.Width >= minWidth && vehicle.Width <= maxWidth {
+			v[key] = vehicle
+		}
+	}
+	return
+}
+
+// FindByWeight is a method that returns a map of vehicles by weight
+func (r *VehicleMap) FindByWeight(minWeight, maxWeight float64) (v map[int]models.Vehicle, err error) {
+	for key, vehicle := range r.db {
+		if vehicle.Weight >= minWeight && vehicle.Weight <= maxWeight {
+			v[key] = vehicle
+		}
+	}
+	return
+}
+
+// Save is a method that saves a vehicle
+func (r *VehicleMap) Save(vehicle models.Vehicle) (v models.Vehicle, err error) {
+
+	// validate if the vehicle already exists
+	_, ok := r.db[vehicle.Id]
+
+	if ok {
+		err = errors.New("vehicle already exists")
+		return
+	}
+
+	return
+}
+
+// SaveAll is a method that saves all vehicles
+func (r *VehicleMap) SaveAll(vehicles []models.Vehicle) (v map[int]models.Vehicle, err error) {
+
+	for _, vehicle := range vehicles {
+		// validate if the vehicle already exists
+		_, ok := r.db[vehicle.Id]
+
+		if ok {
+			err = errors.New("vehicle already exists")
+			return
+		}
+	}
+
+	v = make(map[int]models.Vehicle)
+
+	for _, vehicle := range vehicles {
+		r.db[vehicle.Id] = vehicle
+
+		v[vehicle.Id] = vehicle
+	}
+
+	return
+}
+
+// UpdateMaxSpeedByID is a method that updates the max speed of a vehicle by its ID
+func (r *VehicleMap) UpdateMaxSpeedByID(id int, maxSpeed float64) (v models.Vehicle, err error) {
+	v, ok := r.db[id]
+	if !ok {
+		err = errors.New("vehicle not found")
+		return
+	}
+	v.MaxSpeed = maxSpeed
+	r.db[id] = v
+	return
+}
+
+// UpdateFuelTypeByID is a method that updates the fuel type of vehicle by its ID
+func (r *VehicleMap) UpdateFuelTypeByID(id int, fuelType string) (v models.Vehicle, err error) {
+	v, ok := r.db[id]
+	if !ok {
+		err = errors.New("vehicle not found")
+		return
+	}
+	v.FuelType = fuelType
+	r.db[id] = v
+	return
+}
+
+// RemoveByID is a method that removes a vehicle by its ID
+func (r *VehicleMap) RemoveByID(id int) (err error) {
+	_, ok := r.db[id]
+	if !ok {
+		err = errors.New("vehicle not found")
+		return
+	}
+	delete(r.db, id)
 	return
 }
--- internal/repository/vehicle_repository.go
+++ internal/repository/vehicle_repository.go
@@ -6,4 +6,28 @@ import "github.com/miloalej-dev/vehicle-dealership/pkg/models"
 type VehicleRepository interface {
 	// FindAll is a method that returns a map of all vehicles
 	FindAll() (v map[int]models.Vehicle, err error)
+	// FindByID is a method that returns a vehicle by its ID
+	FindByID(id int) (v models.Vehicle, err error)
+	// FindByBrand is a method that returns a map of vehicles by brand
+	FindByBrand(brand string) (v map[int]models.Vehicle, err error)
+	// FindByTransmission is a method that returns a map of vehicles by transmission
+	FindByTransmission(transmission string) (v map[int]models.Vehicle, err error)
+	// FindByColorAndYear is a method that returns a map of vehicles by color and year
+	FindByColorAndYear(color string, year int) (v map[int]models.Vehicle, err error)
+	// FindByBrandAndBetweenYears is a method that returns a map of vehicles by brand and between years
+	FindByBrandAndBetweenYears(brand string, startYear, endYear int) (v map[int]models.Vehicle, err error)
+	// FindByFuelType is a method that returns a map of vehicles by fuel type
+	FindByFuelType(fuel string) (v map[int]models.Vehicle, err error)
+	// FindByWeight is a method that returns a map of vehicles by weight
+	FindByWeight(minWeight, maxWeight float64) (v map[int]models.Vehicle, err error)
+	// Save is a method that saves a vehicle
+	Save(vehicle models.Vehicle) (v models.Vehicle, err error)
+	// SaveAll is a method that saves all vehicles
+	SaveAll(vehicles []models.Vehicle) (v map[int]models.Vehicle, err error)
+	// UpdateMaxSpeedByID is a method that updates the max speed of a vehicle by its ID
+	UpdateMaxSpeedByID(id int, maxSpeed float64) (v models.Vehicle, err error)
+	// UpdateFuelTypeByID is a method that updates the fuel type of vehicle by its ID
+	UpdateFuelTypeByID(id int, fuelType string) (v models.Vehicle, err error)
+	// RemoveByID is a method that removes a vehicle by its ID
+	RemoveByID(id int) (err error)
 }
