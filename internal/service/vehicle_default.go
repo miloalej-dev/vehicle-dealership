103d63d33415278491e3df4898ae7ce9e59aaa22
Camilo Nossa <camilo.nossa@mercadolibre.com.co>
2025-06-16 11:00:02 -0500

    Add vehicle retrieval, management, and statistics methods to VehicleDefault

--- internal/service/vehicle_default.go
+++ internal/service/vehicle_default.go
@@ -16,8 +16,86 @@ type VehicleDefault struct {
 	rp repository.VehicleRepository
 }
 
-// FindAll is a method that returns a map of all vehicles
-func (s *VehicleDefault) FindAll() (v map[int]models.Vehicle, err error) {
+func (s *VehicleDefault) GetVehicles() (v map[int]models.Vehicle, err error) {
 	v, err = s.rp.FindAll()
+	return
+}
+
+func (s *VehicleDefault) GetVehiclesByColorAndYear(color string, year int) (v map[int]models.Vehicle, err error) {
+	v, err = s.rp.FindByColorAndYear(color, year)
+	return
+}
+
+func (s *VehicleDefault) GetVehiclesByBrandAndBetweenYears(brand string, startYear, endYear int) (v map[int]models.Vehicle, err error) {
+	v, err = s.rp.FindByBrandAndBetweenYears(brand, startYear, endYear)
+	return
+}
+
+func (s *VehicleDefault) GetVehiclesByFuelType(fuelType string) (v map[int]models.Vehicle, err error) {
+	v, err = s.rp.FindByFuelType(fuelType)
+	return
+}
+
+func (s *VehicleDefault) GetVehiclesByTransmission(transmission string) (v map[int]models.Vehicle, err error) {
+	v, err = s.rp.FindByTransmission(transmission)
+	return
+}
+
+func (s *VehicleDefault) GetVehiclesByDimensions(minLength, maxLength, minWidth, maxWidth float64) (v map[int]models.Vehicle, err error) {
+	v, err = s.rp.FindByDimensions(minLength, maxLength, minWidth, maxWidth)
+	return
+}
+
+func (s *VehicleDefault) GetVehiclesByWeight(minWeight, maxWeight float64) (v map[int]models.Vehicle, err error) {
+	v, err = s.rp.FindByWeight(minWeight, maxWeight)
+	return
+}
+
+func (s *VehicleDefault) ChangeMaxSpeed(id int, maxSpeed float64) (v models.Vehicle, err error) {
+	v, err = s.rp.UpdateMaxSpeedByID(id, maxSpeed)
+	return
+}
+
+func (s *VehicleDefault) ChangeFuelType(id int, fuelType string) (v models.Vehicle, err error) {
+	v, err = s.rp.UpdateFuelTypeByID(id, fuelType)
+	return
+}
+
+func (s *VehicleDefault) AverageMaxSpeedByBrand(brand string) (average float64, err error) {
+	v, err := s.rp.FindByBrand(brand)
+	if err != nil {
+		return
+	}
+	for _, vehicle := range v {
+		average += vehicle.MaxSpeed
+	}
+	average /= float64(len(v))
+	return
+}
+
+func (s *VehicleDefault) AverageCapacityByBrand(brand string) (average float64, err error) {
+	v, err := s.rp.FindByBrand(brand)
+	if err != nil {
+		return
+	}
+	for _, vehicle := range v {
+		average += float64(vehicle.Capacity)
+	}
+	average /= float64(len(v))
+	return
+}
+
+func (s *VehicleDefault) AddVehicle(vehicle models.Vehicle) (v models.Vehicle, err error) {
+	v, err = s.rp.Save(vehicle)
+	return
+}
+
+func (s *VehicleDefault) AddVehicles(vehicles []models.Vehicle) (v map[int]models.Vehicle, err error) {
+	v, err = s.rp.SaveAll(vehicles)
+	return
+}
+
+func (s *VehicleDefault) DeleteVehicle(id int) (err error) {
+	err = s.rp.RemoveByID(id)
 	return
 }
--- internal/service/vehicle_service.go
+++ internal/service/vehicle_service.go
@@ -4,6 +4,32 @@ import "github.com/miloalej-dev/vehicle-dealership/pkg/models"
 
 // VehicleService is an interface that represents a vehicle service
 type VehicleService interface {
-	// FindAll is a method that returns a map of all vehicles
-	FindAll() (v map[int]models.Vehicle, err error)
+	// GetVehicles is a method that returns a map of all vehicles
+	GetVehicles() (v map[int]models.Vehicle, err error)
+	// GetVehiclesByColorAndYear is a method that returns a map of vehicles by brand
+	GetVehiclesByColorAndYear(color string, year int) (v map[int]models.Vehicle, err error)
+	// GetVehiclesByBrandAndBetweenYears is a method that returns a map of vehicles by brand and between years
+	GetVehiclesByBrandAndBetweenYears(brand string, startYear, endYear int) (v map[int]models.Vehicle, err error)
+	// GetVehiclesByFuelType is a method that returns a map of vehicles by fuel type
+	GetVehiclesByFuelType(fuelType string) (v map[int]models.Vehicle, err error)
+	// GetVehiclesByTransmission is a method that returns a map of vehicles by transmission
+	GetVehiclesByTransmission(transmission string) (v map[int]models.Vehicle, err error)
+	// GetVehiclesByDimensions is a method that returns a map of vehicles by dimensions
+	GetVehiclesByDimensions(minLength, maxLength, minWidth, maxWidth float64) (v map[int]models.Vehicle, err error)
+	// GetVehiclesByWeight is a method that returns a map of vehicles by weight
+	GetVehiclesByWeight(minWeight, maxWeight float64) (v map[int]models.Vehicle, err error)
+	// ChangeMaxSpeed is a method that changes the max speed of a vehicle by its ID
+	ChangeMaxSpeed(id int, maxSpeed float64) (v models.Vehicle, err error)
+	// ChangeFuelType is a method that changes the fuel type of a vehicle by its ID
+	ChangeFuelType(id int, fuelType string) (v models.Vehicle, err error)
+	// AverageMaxSpeedByBrand is a method that returns the average max speed of vehicles by brand
+	AverageMaxSpeedByBrand(brand string) (average float64, err error)
+	// AverageCapacityByBrand is a method that returns the average capacity of vehicles by brand
+	AverageCapacityByBrand(brand string) (average float64, err error)
+	// AddVehicle is a method that adds a vehicle to the inventory
+	AddVehicle(vehicle models.Vehicle) (v models.Vehicle, err error)
+	// AddVehicles is a method that adds vehicles to the inventory
+	AddVehicles(vehicles []models.Vehicle) (v map[int]models.Vehicle, err error)
+	// DeleteVehicle is a method that deletes a vehicle from the inventory
+	DeleteVehicle(id int) (err error)
 }
